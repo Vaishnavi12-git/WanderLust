b3ec809b4c3cfc4c3ad7761ff2537e329ca3c6c7
Vaishnavi Kale <vaishnavikale06032006@gmail.com>
2026-03-14 10:05:50 +0530

    feat: Add basic navbar styling

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const ejs = require("ejs");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 main().then(() => {
     console.log("Connected");
@@ -73,6 +76,13 @@ app.put("/listings/:id", async(req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async(req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 });
 
 app.get("/", (req, res) => {
