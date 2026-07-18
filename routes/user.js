1fc1533b0aa29e0463080cfd19f45dde0c06bbe3
Vaishnavi Kale <vaishnavikale06032006@gmail.com>
2026-07-19 00:14:55 +0530

    Complete user authentication flow with singup, login, logout and session handling

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/singup", (req, res) => {
     res.render("users/singup.ejs");
@@ -13,9 +14,13 @@ router.post("/singup", wrapAsync(async(req, res) => {
         let {username, email, password} = req.body;
         const newUser = new User({email, username});
         const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.flash("success", "Welcome to Wanderlust");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err);
+            }
+              req.flash("success", "Welcome to Wanderlust");
+              res.redirect("/listings");
+        });
     }catch(e) {
         req.flash("error", e.message);
         res.redirect("/singup");
@@ -27,13 +32,26 @@ router.get("/login", (req, res) => {
 });
 
 router.post("/login",
+    saveRedirectUrl,
     passport.authenticate("local", {
         failureRedirect: "/login",
         failureFlash: true
     }),
     async(req, res) => {
         req.flash("success", "Welcome back to Wanderlust");
-        res.redirect("/listings");
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
 });
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if(err) {
+           return next(err);
+        }
+        req.flash("success", "You are logged out");
+        res.redirect("/listings");
+    })
+});
+
 
 module.exports = router;
